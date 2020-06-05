3db1a3f3bd365c4751ace9eae2b33c5e0a0648b5
GautierCo <gautier.colasse@gmail.com>
2020-06-05 16:34:02 +0200

    Port app.js

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use(express.static('./assets'));
 
 app.use(router);
 
-app.listen(3000, () => {
-    console.log('Lancement du serveur sur le port 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Lancement du serveur sur le port' + process.env.PORT);
 });
